a8510a4638bae438d00128a03d5a12ece9c4a709
Krenar Shatri <krenarkrenarshatri@gmail.com>
2024-02-11 16:40:49 +0100

    Graph store. Some tests

--- src/main/java/org/example/Graph.java
+++ src/main/java/org/example/Graph.java
@@ -1,4 +1,80 @@
 package org.example;
 
+import java.util.*;
+
 public class Graph {
+    // Bidirectional graph
+    public static HashMap<String, HashSet<String>> nodes = new HashMap<>();
+
+    public static void reset() {
+        nodes = new HashMap<>();
+    }
+
+    public static void add(String a, String b) {
+        if (nodes.containsKey(a)) {
+            // Runtime complexity: O(1)
+            nodes.get(a).add(b);
+            nodes.put(b, new HashSet<>(Arrays.asList(a)));
+        } else if (nodes.containsKey(b)) {
+            // Runtime complexity: O(1)
+            nodes.get(b).add(a);
+            nodes.put(a, new HashSet<>(Arrays.asList(b)));
+        } else {
+            // They are new words and can not be attached to some existing node
+            // Go through all the nodes and attach to the one that is an anagram of A or B
+
+            nodes.put(a, new HashSet<>(Arrays.asList(b)));
+            nodes.put(b, new HashSet<>(Arrays.asList(a)));
+
+            // Heavy operation
+            // Runtime complexity: O(|V| * N), where
+            // |V| - number of nodes/vertices
+            // N - length(a)
+            for (String s : nodes.keySet()) {
+                if (Anagrams.areAnagrams(a, s)) {
+                    nodes.get(s).add(a);
+                    nodes.get(a).add(s);
+                }
+            }
+        }
+    }
+
+    public static ArrayList<String> get(String a) {
+        if (nodes.containsKey(a)) return new ArrayList<>(anagramsFor(a));
+        else {
+            // Heavy operation
+            // Runtime complexity: O(|V| * N), where
+            // |V| - number of nodes/vertices
+            // N - length(a)
+            for (String s : nodes.keySet()) {
+                if (Anagrams.areAnagrams(a, s)) {
+                    nodes.get(s).add(a);
+                    nodes.put(a, new HashSet<>(Arrays.asList(s)));
+                    return new ArrayList<>(anagramsFor(a));
+                }
+            }
+        }
+
+        return new ArrayList<>();
+    }
+
+    public static HashSet<String> anagramsFor(String a) {
+        HashSet<String> visited = new HashSet<>();
+        HashSet<String> allAnagrams = new HashSet<>(DFS(a, visited));
+        allAnagrams.remove(a);
+        return allAnagrams;
+    }
+
+    public static HashSet<String> DFS(String currentNode, HashSet<String> visited) {
+        if (visited.contains(currentNode)) return new HashSet<>();
+
+        HashSet<String> newSet = new HashSet<>(List.of(currentNode));
+        visited.add(currentNode);
+
+        for (String s : nodes.get(currentNode)) {
+            newSet.addAll(Graph.DFS(s, visited));
+        }
+
+        return newSet;
+    }
 }
---
+++ src/test/java/GraphTest.java
@@ -0,0 +1,45 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.example.Dictionary;
+import org.example.Graph;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class GraphTest {
+
+    @BeforeEach
+    void clear() {
+        Graph.reset();
+    }
+
+    @Test
+    void whenAnagramsAreNotConnected() {
+        Graph.add("abc", "cba");
+        Graph.add("acb", "cab");
+
+        // HashSet order is not guaranteed, so we sort the result
+        ArrayList<String> expected = new ArrayList<>(Arrays.asList("abc", "cba", "acb", "cab"));
+        ArrayList<String> actual = Graph.get("bac");
+        expected.sort(Comparator.naturalOrder());
+        actual.sort(Comparator.naturalOrder());
+
+        assertEquals(expected, actual);
+    }
+
+    void happyPath() {
+        Graph.add("abc", "cba");
+        Graph.add("cba", "cab");
+
+        // HashSet order is not guaranteed, so we sort the result
+        ArrayList<String> expected = new ArrayList<>(Arrays.asList("abc", "cba", "cba", "cab"));
+        ArrayList<String> actual = Graph.get("abc");
+        expected.sort(Comparator.naturalOrder());
+        actual.sort(Comparator.naturalOrder());
+
+        assertEquals(expected, actual);
+    }
+}
