c0702c4f05a7c782a61338292942378fba2098dc
Krenar Shatri <krenarkrenarshatri@gmail.com>
2024-02-11 15:49:07 +0100

    Dictionary for storing anagrams. Simple tests

--- src/main/java/org/example/Dictionary.java
+++ src/main/java/org/example/Dictionary.java
@@ -1,4 +1,38 @@
 package org.example;
 
+import java.util.*;
+
 public class Dictionary {
+
+    // ("abc", "acb", "bca") -> ("abc" -> ("abc", "acb", "bca"))
+    private static HashMap<String, HashSet<String>> dictionary = new HashMap<>();
+
+    public static void add(String a, String b) {
+        // We assume that A and B are anagrams
+        // Use the sorted string as the key
+        // All anagrams will return the same sorted string
+        char[] charArrayA = a.toCharArray();
+        // O(N*log(N)), where N is length(A)
+        Arrays.sort(charArrayA);
+        String sortedA = new String(charArrayA);
+
+        if(dictionary.containsKey(sortedA)) dictionary.get(sortedA).addAll(List.of(a,b));
+        else dictionary.put(sortedA, new HashSet(List.of(a, b)));
+    }
+
+    public static ArrayList<String> get(String a) {
+       char[] charArrayA = a.toCharArray();
+       Arrays.sort(charArrayA);
+       String sortedA = new String(charArrayA);
+
+       if(!dictionary.containsKey(sortedA)) return new ArrayList<>();
+
+       ArrayList<String> result = new ArrayList<>();
+
+        for(String s : dictionary.get(sortedA)) {
+            if(!s.equals(a)) result.add(s);
+        }
+
+       return result;
+    }
 }
---
+++ src/test/java/DictionaryTest.java
@@ -0,0 +1,35 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.example.Dictionary;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class DictionaryTest {
+
+    @Test
+    void testAddPairHappyPath() {
+        Dictionary.add("abc", "cba");
+        Dictionary.add("acb", "cab");
+
+        // HashSet order is not guaranteed, so we sort the result
+        ArrayList<String> expected = new ArrayList<>(Arrays.asList("abc", "cba", "acb", "cab"));
+        ArrayList<String> actual = Dictionary.get("bac");
+        expected.sort(Comparator.naturalOrder());
+        actual.sort(Comparator.naturalOrder());
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void differentStrings() {
+        // The dictionary doesn't check if they are anagrams
+        // It just stores
+        Dictionary.add("cba", "xyz");
+        assertEquals(List.of("xyz"), Dictionary.get("cba"));
+    }
+
+}
